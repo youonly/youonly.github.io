8a04bd5539c5ba42e775c93c308bc94280606803
Mihir Dave <yyuuonly@gmail.com>
2016-11-17 20:24:50 +0530

    Initial commit

--- www.spotebi.com/wp-content/gif-timer/js/interval-timer.js
+++ www.spotebi.com/wp-content/gif-timer/js/interval-timer.js
@@ -1,5 +1,5 @@
-var source="http://www.awesomex.tk/www.spotebi.com/wp-content/gif-timer/resources/sounds/get-ready.mp3";
-
+var source="";//"http://www.awesomex.tk/www.spotebi.com/wp-content/gif-timer/resources/sounds/get-ready.mp3";
+var x=1;
 /*global window, document, BLOCKS */
 	// Create audio context
 	var ctx;
@@ -65,51 +65,7 @@ var simpleWebAudioPlayer = function () {
 
 
 
-//$(window).ready(function(){$(".timer-next").on("touchend",
-function onWindowLoad(){var button = document.getElementsByClassName("timer-next")[0],
-		player = simpleWebAudioPlayer(),
-		soundLoaded = false,
-		
-		playTestSound = function () {
-			player.play("test");
-		},
-		
-		onSoundLoaded = function () {
-			soundLoaded = true;
-			playTestSound();
-		};
-		
-		buttonTapped = function () {
-			
-			if (soundLoaded) {
-				playTestSound();
-			} else {
-				player.load({
-					name: "test",
-					src: source,
-					callback: onSoundLoaded
-				});
-			}
-		};
-	if ("ontouchstart" in window) {
-		button.addEventListener("touchstart", buttonTapped);
-	//alert("1");
-	} else {
-		button.addEventListener("mousedown", buttonTapped);
-	//alert("2");
-	}
-            }//)});
 
-
-
-
-	if (window.addEventListener) {
-		window.addEventListener("load", onWindowLoad, false);
-	} else if (window.attachEvent) {
-		window.attachEvent("onload", onWindowLoad);
-	} else {
-		window.onload = onWindowLoad;
-	}
 			
 
 function onWindowLoad2(){var player = simpleWebAudioPlayer(),
