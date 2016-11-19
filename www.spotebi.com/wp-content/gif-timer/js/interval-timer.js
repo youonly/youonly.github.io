1d428c69d63bd8415400e9bcbdbbc291b0e91fee
Mihir Dave <yyuuonly@gmail.com>
2016-11-19 17:18:55 +0530

    Initial commit

--- www.spotebi.com/wp-content/gif-timer/js/interval-timer.js
+++ www.spotebi.com/wp-content/gif-timer/js/interval-timer.js
@@ -93,21 +93,45 @@ function onWindowLoad(){var button = document.getElementsByClassName("timer-menu
 		};
 	if ("ontouchstart" in window) {
 		button.addEventListener("touchstart", buttonTapped);
-	//alert("1");
+
 	} else {
 		button.addEventListener("mousedown", buttonTapped);
-	//alert("2");
+
 	}
             }//)});
+function hello(){var button = document.getElementsByClassName("timer-menu")[0],
+		player = simpleWebAudioPlayer(),
+		soundLoaded = false,
+		
+		playTestSound = function () {
+			player.play("test");
+		},
+		
+		onSoundLoaded = function () {
+			soundLoaded = true;
+			playTestSound();
+		};
+		
+		buttonTapped = function () {
+			
+			if (soundLoaded) {
+				playTestSound();
+			} else {
+				player.load({
+					name: "test",
+					src: source,
+					callback: onSoundLoaded
+				});
+			}
+		};}
 
 
 
-
-	/*if (window.addEventListener) {
-		window.addEventListener("load", onWindowLoad, false);
+	if (window.addEventListener) {
+		window.addEventListener("load", hello, false);
 	} else if (window.attachEvent) {
-		window.attachEvent("onload", onWindowLoad);
+		window.attachEvent("onload", hello);
 	} else {
-		window.onload = onWindowLoad;
-	}*/
+		window.onload = hello;
+	}
 			
