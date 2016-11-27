89217e81b54328738d3272877ce7b5a1e0003b74
Mihir Dave <yyuuonly@gmail.com>
2016-11-27 17:36:14 +0530

    Initial commit

--- www.spotebi.com/wp-content/gif-timer/js/custom_player.js
+++ www.spotebi.com/wp-content/gif-timer/js/custom_player.js
@@ -28,7 +28,7 @@ var divx = document.createElement('div');
 my_elem.parentNode.insertBefore(divx, my_elem.nextSibling);
 divx.style.marginTop = "2px";
 divx.style.borderBottom = "thick solid #0f0f0f";
-document.getElementById("soundcloudControls").innerHTML = '<div class="playControls__controls"><button class="skipControl playControls__icon sc-ir skipControl__previous">Skip to previous</button><button class="playControl playControls__icon sc-ir">Play current</button><button class="skipControl playControls__icon sc-ir skipControl__next">Skip to next</button><button class="volume__button volume__speakerIcon sc-ir" data-level="10">Toggle mute</button><div class="slider ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"><div class="ui-slider-range ui-widget-header ui-corner-all ui-slider-range-min" style="width: 100%; background: rgb(51, 51, 51);"></div><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 100%;"></span></div></div>';
+document.getElementById("soundcloudControls").innerHTML = '<div id="can_hide"><div class="playControls__controls"><button class="skipControl playControls__icon sc-ir skipControl__previous">Skip to previous</button><button class="playControl playControls__icon sc-ir">Play current</button><button class="skipControl playControls__icon sc-ir skipControl__next">Skip to next</button><button class="volume__button volume__speakerIcon sc-ir" data-level="10">Toggle mute</button><div class="slider ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"><div class="ui-slider-range ui-widget-header ui-corner-all ui-slider-range-min" style="width: 100%; background: rgb(51, 51, 51);"></div><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 100%;"></span></div></div>Loading...</div>';
 $("#soundcloudControls").soundCloudControls("soundcloud", "#333");
 
 
@@ -39,7 +39,7 @@ var audio_timer = document.getElementById("audio-sounds");
   console.log("2 -->"+document.getElementById("soundcloudControls").innerHTML);
   widget.bind(SC.Widget.Events.READY, function() {
     console.log('Ready...');
-    document.getElementById("soundcloudControls").innerHTML = '<div class="playControls__controls"><button class="skipControl playControls__icon sc-ir skipControl__previous">Skip to previous</button><button class="playControl playControls__icon sc-ir">Play current</button><button class="skipControl playControls__icon sc-ir skipControl__next">Skip to next</button><button class="volume__button volume__speakerIcon sc-ir" data-level="10">Toggle mute</button><div class="slider ui-slider ui-slider-horizontal ui-widget ui-widget-content ui-corner-all"><div class="ui-slider-range ui-widget-header ui-corner-all ui-slider-range-min" style="width: 100%; background: rgb(51, 51, 51);"></div><span class="ui-slider-handle ui-state-default ui-corner-all" tabindex="0" style="left: 100%;"></span></div></div>';
+    document.getElementById("can_hide").style.display = none;
     console.log("3 -->"+document.getElementById("soundcloudControls").innerHTML);
 
 if(ua.iOS){console.log("checkpt1");
