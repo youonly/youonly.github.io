c8acc912ed8e5e84c15d5df119f0b00ee72db8eb
youonly <yyuuonly@gmail.com>
2020-07-20 11:13:33 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -196,6 +196,7 @@ function heightAdj() {
       var xxHeight = $(this).height();
       var xxWidth = $(this).width();
       $(this).css('left',-(xxWidth/2)+'px');
+      $(this).css('top',xxHeight/2 +'px');
   });
 }
 
